a358307d14bddca0af3b07db0f5a6167d395234d
Antoine Hanna <hannaantoinebusiness@gmail.com>
2025-01-28 15:53:23 +0100

    [ADD] AddNodeBookEntryNode

--- Assets/LittleDialogue/Runtime/LittleGraphAddOn/LDNoChoiceDialogueNode.cs
+++ Assets/LittleDialogue/Runtime/LittleGraphAddOn/LDNoChoiceDialogueNode.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 namespace LittleDialogue.Runtime.LittleGraphAddOn
 {
-    [LGNodeInfo("No Choice Dialogue", "Little Dialogue/No Choice Dialogue", true, true)]
+    [LGNodeInfo("No Choice Dialogue", "Little Dialogue/No Choice Dialogue")]
     public class LDNoChoiceDialogueNode : LDDialogueNode
     {
         
---
+++ Assets/_Project/Scripts/RedactionSystem/LittleGraphAddOn/AddNoteBookEntryNode.cs
@@ -0,0 +1,37 @@
+using LittleGraph.Runtime;
+using LittleGraph.Runtime.Attributes;
+using UnityEngine;
+
+namespace _Project.Scripts.RedactionSystem.LittleGraphAddOn
+{
+#if LITTLE_GRAPH
+    
+    [LGNodeInfo("Add Notebook Entry", "Redaction System/Add Notebook Entry")]
+    public class AddNoteBookEntryNode : LGNode
+    {
+        [ExposedProperty] public string NotebookText = "Notebook Text";
+        [ExposedProperty] public string ArticleText = "Article Text";
+        [ExposedProperty] public float PeopleAppreciation = 1.0f;
+        [ExposedProperty] public float RebelsAppreciation = 1.0f;
+        [ExposedProperty] public float GovernmentAppreciation = 1.0f;
+        
+        protected override void ExecuteNode()
+        {
+            if(NoteBook.instance == null)
+            {
+                Debug.LogError("Add Notebook Entry Node : No instance of Notebook");
+                return;
+            }
+            NoteBook.instance.AddEntry(NotebookText, ArticleText, RebelsAppreciation, PeopleAppreciation, GovernmentAppreciation);
+
+            if (m_nodeConnections.Exists(connection => connection.OutputPort.NodeId == ID))
+            {
+                LGConnection connection = m_nodeConnections.Find(connection => connection.OutputPort.NodeId == ID);
+                EmitFlow(connection.InputPort.NodeId);
+            }
+            
+            base.ExecuteNode();
+        }
+    }
+#endif
+}
