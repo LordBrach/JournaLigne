11bc1d1ac4abb0c543436b8e7e337a4c21ee0106
Antoine Hanna <hannaantoinebusiness@gmail.com>
2025-01-29 10:38:28 +0100

    [ADD] GameScene, MainMenuScene, GameManager, StoryManager

---
+++ Assets/_Project/Scripts/Runtime/Core/GameManager.cs
@@ -0,0 +1,38 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace _Project.Scripts.Runtime.Core
+{
+    public class GameManager : MonoBehaviour
+    {
+        #region Singleton
+        private static GameManager m_instance;
+        public static GameManager Instance => m_instance;
+
+        private void InitSingleton()
+        {
+            if (m_instance != null && m_instance != this)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                m_instance = this;
+                DontDestroyOnLoad(gameObject);
+            } 
+        }
+        #endregion
+        
+        private void Awake()
+        {
+            InitSingleton();
+        }
+
+        //Scene must be in build settings
+        public void LoadScene(string sceneName)
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/Runtime/Story/StoryManager.cs
@@ -0,0 +1,34 @@
+using System;
+using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Story
+{
+    public class StoryManager : MonoBehaviour
+    {
+        #region Singleton
+        private static StoryManager m_instance;
+        public static StoryManager Instance => m_instance;
+        private void InitSingleton()
+        {
+            if (m_instance != null && m_instance != this)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                m_instance = this;
+            } 
+        }
+        #endregion
+        
+        private void Awake()
+        {
+            InitSingleton();
+        }
+
+        private void Start()
+        {
+            
+        }
+    }
+}
