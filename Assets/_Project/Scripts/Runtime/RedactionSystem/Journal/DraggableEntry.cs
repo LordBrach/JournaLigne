7de94da96b3cdbfb4c80f153eede164ebd8a0530
bixboy <pierrelamare62@gmail.com>
2025-01-29 15:12:38 +0100

    [MOD] News Paper Drag copy

--- Assets/_Project/Scripts/Runtime/RedactionSystem/Journal/DraggableEntry.cs
+++ Assets/_Project/Scripts/Runtime/RedactionSystem/Journal/DraggableEntry.cs
@@ -7,8 +7,10 @@ public class DraggableEntry : MonoBehaviour, IBeginDragHandler, IDragHandler, IE
     private RectTransform _rectTransform;
     private CanvasGroup _canvasGroup;
     private Vector3 _originalPosition;
+    public bool isUsed = false;
 
     public String text;
+    private GameObject _cloneInstance;
 
     void Awake()
     {
@@ -18,20 +20,24 @@ public class DraggableEntry : MonoBehaviour, IBeginDragHandler, IDragHandler, IE
 
     public void OnBeginDrag(PointerEventData eventData)
     {
+        if (isUsed) return;
+        
         _originalPosition = _rectTransform.position;
         _canvasGroup.alpha = 0.6f;
         _canvasGroup.blocksRaycasts = false;
-
+        CreateClone();
     }
 
     public void OnDrag(PointerEventData eventData)
     {
+        if (isUsed) return;
         _rectTransform.anchoredPosition += eventData.delta / transform.root.GetComponent<Canvas>().scaleFactor;
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
         _canvasGroup.alpha = 1f;
+        Destroy(_cloneInstance);
 
         if (eventData.pointerDrag == null || eventData.pointerDrag.GetComponent<EntrySlot>() == null)
         {
@@ -39,6 +45,26 @@ public class DraggableEntry : MonoBehaviour, IBeginDragHandler, IDragHandler, IE
             _rectTransform.position = _originalPosition;
         }
         
+    }
+    
+    private void CreateClone()
+    {
+        int originalIndex = transform.GetSiblingIndex();
+        _cloneInstance = Instantiate(gameObject, transform.parent);
+        _cloneInstance.transform.SetSiblingIndex(originalIndex);
+
+        var cloneDraggable = _cloneInstance.GetComponent<DraggableEntry>();
+        if (cloneDraggable != null)
+        {
+            Destroy(cloneDraggable);
+        }
+
+        var cloneCanvasGroup = _cloneInstance.GetComponent<CanvasGroup>();
+        if (cloneCanvasGroup != null)
+        {
+            cloneCanvasGroup.alpha = 1f;
+            cloneCanvasGroup.blocksRaycasts = false;
+        }
     }
     
 }
